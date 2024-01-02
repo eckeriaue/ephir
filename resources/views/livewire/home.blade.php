162df2bf5270974c85f7709059105b0f1774ee11
metalhead <114188050+eckeriaue@users.noreply.github.com>
2024-01-03 01:55:03 +0300

    сделал элемент загрузки постов

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -17,13 +17,16 @@ mount($getPosts);
 ?>
 
 
-    <section class="mx-auto mt-8 max-w-full xl:max-w-4xl px-4">
+    <section
+    class="mx-auto mt-8 max-w-full xl:max-w-4xl px-4">
 
         @auth
             <livewire:create-post-modal @save="getPosts" />
         @endauth
 
+        
         <section>
+            <div wire:loading> Загрузка постов... </div>
             @foreach($posts as $post)
             <article wire:key="{{ $post->id }}">
                 {{$post}}
