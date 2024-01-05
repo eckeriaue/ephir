9919bfc059dbc3665cd136298ae8706d44d750c1
l <eckeriaue@gmail.com>
2024-01-06 00:59:15 +0300

    mvp

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 Volt::route('/', 'home')->name('home');
 
 Route::view('profile', 'profile')
