43b3a85f18731bd5685120ce3380711eff0262f1
metalhead <114188050+eckeriaue@users.noreply.github.com>
2024-01-06 17:33:35 +0300

    начал писать авторизацию

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function __invoke(Request $request)
+    {
+        $offset = (int)$request->query('offset', 0);
+        $limit = (int)$request->query('limit',10);
+        $posts = Post::query()
+            ->orderBy('created_at','desc')
+            ->where('is_published', '>', 0)
+            ->offset($offset)
+            ->limit($limit)
+            ->get(['id','title','content','created_at']);
+        return $posts;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
