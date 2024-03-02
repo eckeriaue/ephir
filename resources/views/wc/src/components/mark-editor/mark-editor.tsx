a63cc5f03d62c2afae5d9132d27ab58cf7510168
metalhead <114188050+eckeriaue@users.noreply.github.com>
2024-03-03 01:20:36 +0300

    file uploads

--- resources/views/wc/src/components/mark-editor/mark-editor.tsx
+++ resources/views/wc/src/components/mark-editor/mark-editor.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, State, h } from '@stencil/core';
 
 @Component({
   tag: 'mark-editor',
@@ -7,15 +7,33 @@ import { Component, h } from '@stencil/core';
 })
 export class MarkEditor {
 
+  content: HTMLParagraphElement
+
+  addImage(file: File) {
+    return Object.assign(new Image(), {
+      src: URL.createObjectURL(file)
+    })
+  }
+
+  openFilePicker() {
+    const input = Object.assign(document.createElement('input'), {
+      type: 'file',
+      accept:'image/png, image/gif, image/jpeg' 
+    })
+    input.addEventListener('input', (event) => {
+      const file = (event.target as HTMLInputElement).files.item(0)
+      this.content.appendChild(this.addImage(file))
+    }, { once: true })
+    input.click()
+  }
 
   render() {
     return (
-      <div>
-        <input />
-
-        <p class="p-4 border" contenteditable="plaintext-only">
+      <fieldset>
+        <button type='button' onClick={() => this.openFilePicker()} class="load-photo">{'📸'}</button>
+        <p class="content" ref={el => this.content = el} contenteditable="plaintext-only">
         </p>
-      </div>
+      </fieldset>
     );
   }
 }
