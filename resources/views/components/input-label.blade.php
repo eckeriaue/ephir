7e1a2395befff4b09caa6a5c429bd64f0a2e8fc4
Акромат <108363130+Akromat1@users.noreply.github.com>
2024-03-03 18:47:34 +0300

    Анимация для импута

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,6 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+<label {{ $attributes->merge(['class' => 'transition-all text-[#4A90E2] hover:scale-[1.1] block font-medium text-sm text-gray-700']) }}>
     {{ $value ?? $slot }}
 </label>
+
