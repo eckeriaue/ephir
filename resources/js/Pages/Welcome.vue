83bd003a87a452b8d37d1e2b591a354cca8eeaab
metalhead <114188050+eckeriaue@users.noreply.github.com>
2024-05-01 00:42:13 +0300

    убрал точки с запятой

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3'
 
 defineProps<{
-    canLogin?: boolean;
-    canRegister?: boolean;
-    laravelVersion: string;
-    phpVersion: string;
-}>();
+    canLogin?: boolean
+    canRegister?: boolean
+    laravelVersion: string
+    phpVersion: string
+}>()
 
 function handleImageError() {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-    document.getElementById('background')?.classList.add('!hidden');
+    document.getElementById('screenshot-container')?.classList.add('!hidden')
+    document.getElementById('docs-card')?.classList.add('!row-span-1')
+    document.getElementById('docs-card-content')?.classList.add('!flex-row')
+    document.getElementById('background')?.classList.add('!hidden')
 }
 </script>
 
