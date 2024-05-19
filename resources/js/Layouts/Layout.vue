f0c63848a877a23de38c469cf002390f8796eed9
metalhead <114188050+eckeriaue@users.noreply.github.com>
2024-05-19 21:39:31 +0300

    добавил свг аватар плейсхолдер

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -53,12 +53,22 @@ const logoutForm = useForm({})
 
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
                             <!-- Settings Dropdown -->
-                            <div class="ms-3 relative">
+                            <div class="ms-3 relative space-x-2">
                                 <create-post-modal />
                                 <kit-dropdown-menu>
                                     <kit-dropdown-trigger>
-                                            <kit-button v-if="$page.props.auth.user" variant="ghost" type="button" class="font-medium text-base text-gray-800">
+                                            <kit-button v-if="$page.props.auth.user" variant="ghost" type="button" class="font-medium inline-flex gap-x-2 text-base text-gray-800">
                                                     {{ $page.props.auth.user?.name }}
+                                                    <img
+                                                        v-if="
+                                                            'avatar' in $page.props.auth.user &&
+                                                            typeof $page.props.auth.user.avatar === 'string' 
+                                                        "
+                                                        :src="$page.props.auth.user.avatar"
+                                                    />
+                                                    <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                    </svg>
                                             </kit-button>
                                             <kit-button v-else variant="ghost" type="button" class="font-medium text-base text-gray-800">
                                                 Гость
