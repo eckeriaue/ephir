fa11f6a479dee8a633b7271900e2a68ddb4d9d52
l <eckeriaue@gmail.com>
2024-11-01 00:46:04 +0300

    make indicator

---
+++ packages/wc/src/components/WcSelectmenuIndicator.ce.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { inject } from 'vue'
+const { isOpen } = inject(`selectmenu`)
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24" 
+    stroke-width="1.5"
+    stroke="currentColor"
+    :style="isOpen && {
+      rotate: `180deg`,
+    }"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+  </svg>
+</template>
+
+<style scoped>
+svg {
+  width: 16px;
+  height: 16px;
+}
+</style>
--- packages/wc/src/components/WcSelectmenuSheet.ce.vue
+++ packages/wc/src/components/WcSelectmenuSheet.ce.vue
@@ -1,12 +1,29 @@
 <script setup lang="ts">
-import { inject, ref, useTemplateRef } from 'vue'
-import {useFloating} from '@floating-ui/vue'
+import { offset, shift, autoPlacement, useFloating } from '@floating-ui/vue'
+import { computed, inject, useTemplateRef } from 'vue'
+import { trim } from 'radashi'
 
+const props = defineProps({
+  placements: String
+})
+
+const placements = computed(() => {
+  if (props.placements) {
+    return props.placements.split(',').map(placement => trim(placement))
+  }
+})
 const { isOpen, reference } = inject(`selectmenu`)
 
 const floating = useTemplateRef('floating')
-const { floatingStyles } = useFloating(reference, floating)
-
+const { floatingStyles } = useFloating(reference, floating, {
+  middleware: computed(() => [
+    offset(4),
+    shift(),
+    autoPlacement({
+      allowedPlacements: placements.value,
+    }),
+  ])
+})
 </script>
 
 <template>
