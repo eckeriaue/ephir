d54d948cba71a880e1c99603eff2ccbe2caff055
l <eckeriaue@gmail.com>
2024-11-15 01:50:27 +0300

    set index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,6 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
