ab6fe4d820d1a1bad275b1f98b0923bf1f3d99aa
l <eckeriaue@gmail.com>
2024-11-15 01:10:20 +0300

    init blade only

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
-import './bootstrap';
+import 'htmx.org'
+import _hyperscript from 'hyperscript.org'
+_hyperscript.browserInit()
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+// delete this
+import './bootstrap'
+import Alpine from 'alpinejs'
+window.Alpine = Alpine
+Alpine.start()
