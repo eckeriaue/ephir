ee9626f136440604af1dcd54f96e8286321b1431
l <eckeriaue@gmail.com>
2024-12-28 19:29:00 +0300

    feat: make better inputs

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,10 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
+<label
+    {{ $attributes->merge([
+        'class' => 'text-#1F2328 block font-600 text-14px'
+    ])
+    }}
+>
     {{ $value ?? $slot }}
 </label>
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,15 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input
+    @disabled($disabled)
+    {{ $attributes->merge([
+        'class' => "
+            outline-2 px-2 outline-solid
+            outline-#D1D9E0 h-8
+            focus:outline-#0969DA
+            rounded-6px
+            text-14px
+            placeholder:text-#59636E
+        "
+    ]) }}
+>
