8f301d296a82180ab970f27816f0f98be3b817a4
l <eckeriaue@gmail.com>
2025-01-04 04:19:05 +0300

    feat: make better i18n

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -1,39 +1,131 @@
 
 import EditorJS from '@editorjs/editorjs'
+
+const holder = document.getElementById('editor')
 const editor = new EditorJS({
-    holder: 'editor',
+    holder,
+    /**
+     * Tools list
+     */
+    // tools: {
+    //   header: Header,
+    //   image: SimpleImage,
+    //   list: List,
+    //   checklist: Checklist,
+    //   quote: Quote,
+    //   warning: Warning,
+    //   marker: Marker,
+    //   code: CodeTool,
+    //   delimiter: Delimiter,
+    //   inlineCode: InlineCode,
+    //   linkTool: LinkTool,
+    //   embed: Embed,
+    //   table: Table
+    // },
+
+    /**
+     * Internationalzation config
+     */
     i18n: {
-        messages: {
-            toolNames: {
-                "Text": "Параграф",
-                "Heading": "Заголовок",
-                "List": "Список",
+      /**
+       * @type {I18nDictionary}
+       */
+      messages: {
+        /**
+         * Other below: translation of different UI components of the editor.js core
+         */
+        ui: {
+          "blockTunes": {
+            "toggler": {
+              "Click to tune": "Нажмите, чтобы настроить",
+              "or drag to move": "или перетащите"
             },
-            ui: {
-            "toolbar": {
-                "toolbox": {
-                "Add": "Добавить"
-                }
-            },
-            "blockTunes": {
-                "toggler": {
-                        "Click to tune": "Нажмите, чтобы настроить",
-                        "or drag to move": "Или перетащите, чтобы переместить"
-                    }
-                },
-            },
-            tools: { "warning": { "Title": "Название", "Message": "Сообщение", }, "link": { "Add a link": "Вставьте ссылку" }, },
-            blockTunes: {
-                delete: {
-                    Delete: "Удалить"
-                },
-                moveUp: {
-                    "Move up": "Переместить вверх"
-                },
-                moveDown: {
-                    "Move down": "Переместить вниз"
-                }
+          },
+          "inlineToolbar": {
+            "converter": {
+              "Convert to": "Конвертировать в"
             }
-        }
-    }
+          },
+          "toolbar": {
+            "toolbox": {
+              "Add": "Добавить"
+            }
+          }
+        },
+
+        /**
+         * Section for translation Tool Names: both block and inline tools
+         */
+        toolNames: {
+          "Text": "Параграф",
+          "Heading": "Заголовок",
+          "List": "Список",
+          "Warning": "Примечание",
+          "Checklist": "Чеклист",
+          "Quote": "Цитата",
+          "Code": "Код",
+          "Delimiter": "Разделитель",
+          "Raw HTML": "HTML-фрагмент",
+          "Table": "Таблица",
+          "Link": "Ссылка",
+          "Marker": "Маркер",
+          "Bold": "Полужирный",
+          "Italic": "Курсив",
+          "InlineCode": "Моноширинный",
+        },
+
+        /**
+         * Section for passing translations to the external tools classes
+         */
+        tools: {
+          /**
+           * Each subsection is the i18n dictionary that will be passed to the corresponded plugin
+           * The name of a plugin should be equal the name you specify in the 'tool' section for that plugin
+           */
+          "warning": { // <-- 'Warning' tool will accept this dictionary section
+            "Title": "Название",
+            "Message": "Сообщение",
+          },
+
+          /**
+           * Link is the internal Inline Tool
+           */
+          "link": {
+            "Add a link": "Вставьте ссылку"
+          },
+          /**
+           * The "stub" is an internal block tool, used to fit blocks that does not have the corresponded plugin
+           */
+          "stub": {
+            'The block can not be displayed correctly.': 'Блок не может быть отображен'
+          }
+        },
+
+        /**
+         * Section allows to translate Block Tunes
+         */
+        blockTunes: {
+          /**
+           * Each subsection is the i18n dictionary that will be passed to the corresponded Block Tune plugin
+           * The name of a plugin should be equal the name you specify in the 'tunes' section for that plugin
+           *
+           * Also, there are few internal block tunes: "delete", "moveUp" and "moveDown"
+           */
+          "delete": {
+            "Delete": "Удалить"
+          },
+          "moveUp": {
+            "Move up": "Переместить вверх"
+          },
+          "moveDown": {
+            "Move down": "Переместить вниз"
+          }
+        },
+      }
+    },
+  });
+
+Reflect.set(holder, '$editor', editor)
+editor.isReady.then(() => {
+    editor.focus()
 })
