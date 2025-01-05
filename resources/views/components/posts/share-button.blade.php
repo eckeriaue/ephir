955a62431cc1b4296fe993980523b7fefb70f3f0
metalhead <eckeriaue@gmail.com>
2025-01-05 23:01:55 +0300

    feat: make share button

---
+++ resources/views/components/posts/share-button.blade.php
@@ -0,0 +1,24 @@
+@props(['post'])
+
+<div {{ $attributes->merge() }} x-data="{ isOpen: false }" @click.outside="isOpen = false">
+        <x-button.text @click="isOpen = !isOpen" x-ref="shareButton">
+            <span class="text-16px pr-2 ph ph-share"></span>
+            <span>Поделиться</span>
+        </x-button.text>
+        <x-menu
+            x-anchor.offset.4="$refs.shareButton"
+            x-show="isOpen"
+            @mouseup="isOpen = false"
+        >
+            <x-button.text>Опубликовать у себя</x-button.text>
+            <x-button.text
+                type="button"
+                data-post-href="{{ route('posts.read', ['id' => $post->id]) }}"
+                @click="$clipboard($event.target.dataset.postHref).then(() => {
+                    alert('скопировано')
+                })"
+            >
+                Копировать ссылку
+            </x-button.text>
+        </x-menu>
+    </div>
