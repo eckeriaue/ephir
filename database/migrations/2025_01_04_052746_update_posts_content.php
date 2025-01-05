385ebd4f9afd18250e5d5a12839aa83bf98f6590
l <eckeriaue@gmail.com>
2025-01-05 21:25:01 +0300

    refactor: make drop migrations

--- database/migrations/2025_01_04_052746_update_posts_content.php
+++ database/migrations/2025_01_04_052746_update_posts_content.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Nette\Schema\Schema as NetteSchema;
 
 return new class extends Migration
 {
@@ -23,6 +24,11 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('users', function(Blueprint $table) {
+            $table->text('content');
+            $table->dropColumn('content_json');
+            $table->dropColumn('content_html');
+        });
         //
     }
 };
