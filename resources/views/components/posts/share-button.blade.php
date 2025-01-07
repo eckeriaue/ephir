731f6cc17cf0ed49bc9878b3f9e300baabd75f1e
metalhead <eckeriaue@gmail.com>
2025-01-07 19:51:09 +0300

    chore: publish if auth only

--- resources/views/components/posts/share-button.blade.php
+++ resources/views/components/posts/share-button.blade.php
@@ -11,13 +11,15 @@
             x-show="isOpen"
             @mouseup="isOpen = false"
         >
-            <x-button.text
-                data-hx-patch="{{ route('api.profile.share-post', ['postId' => $post->id]) }}"
-                data-hx-target="body"
-                data-hx-swap="beforeend"
-            >
-                Опубликовать у себя
-            </x-button.text>
+            @auth
+                <x-button.text
+                    data-hx-patch="{{ route('api.profile.share-post', ['postId' => $post->id]) }}"
+                    data-hx-target="body"
+                    data-hx-swap="beforeend"
+                >
+                    Опубликовать у себя
+                </x-button.text>
+            @endauth
             <x-button.text
                 type="button"
                 data-post-href="{{ route('posts.read', ['id' => $post->id]) }}"
