7a9113ebe0e1deee3d74faf5b6f6da42a66a274e
metalhead <eckeriaue@gmail.com>
2025-01-08 06:31:41 +0300

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/editor/index.js',
+                'resources/js/editor.js',
                 'resources/js/slider.js'
             ],
             refresh: true,
