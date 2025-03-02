cb765e79cba780d1a9716192a54ff8cd5fe1b300
l <eckeriaue@gmail.com>
2025-03-02 18:08:47 +0300

    feat: hx image preview image positions

--- resources/views/hx/image-preview.blade.php
+++ resources/views/hx/image-preview.blade.php
@@ -5,31 +5,41 @@ $pid = "imagePreviewId".uniqid();
 $image = PostImage::find($imageId);
 @endphp
 
+<script type="text/hyperscript">
+behavior RemoveImagePreview
+  on click
+    add .scale-0 to #imageWrapperId{{$pid}} then
+    add .opacity-0 to #{{$pid}} then
+    wait 150ms then
+    remove #{{$pid}}
+  end
+end
+</script>
+
 <div
   id="{{$pid}}"
   data-from="imageId{{$image->id}}"
-  _="
+  class="isolate fixed inset-0 z-11 h-dvh w-full opacity-0 transition-opacity"
+  data-script="
     init
-      measure #imageId{{$image->id}} then
-      set :rect to it then
-      set :wrapper to #imageWrapperId{{$pid}}
-      set :wrapper.style.top to :rect.top + 'px' then
-      set :wrapper.style.left to :rect.left + 'px' then
-      set :wrapper.style.width to :rect.width + 'px' then
-      set :wrapper.style.height to :rect.height + 'px' then
-      remove .opacity-0 from :wrapper then
+      remove .opacity-0 from me then
+      remove .scale-0 from #imageWrapperId{{$pid}} then
     end
   "
-  class="isolate fixed inset-0 z-11 h-dvh w-full"
 >
-  <div class="fixed inset-0 size-full bg-black opacity-80"></div>
+  <div class="fixed inset-0 size-full bg-black opacity-80" data-script="install RemoveImagePreview"></div>
   <button
     type="button"
-    class="ph-bold ph-x fixed right-2 top-2 cursor-pointer z-13 p-2 bg-white rounded"
-    data-script="on click remove #{{$pid}}"></button>
+    class="ph-bold ph-x fixed right-2 top-2 shadow-sm cursor-pointer z-13 p-2 bg-white rounded"
+    data-script="install RemoveImagePreview"></button>
 
-    <div class="z-12 fixed inset-0 opacity-0" id="imageWrapperId{{$pid}}">
-      <img src="{{$image->src}}" >
+    <div class="z-12 fixed inset-0 overflow-auto h-dvh">
+      <div
+        class="scale-0 transition-transform duration-300"
+        id="imageWrapperId{{$pid}}"
+      >
+        <img class="p-8" src="{{$image->src}}">
+      </div>
     </div>
 
 </div>
